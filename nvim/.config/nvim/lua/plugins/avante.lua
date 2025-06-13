db87869f992fc38f76b3feb077264ed9ea0c0ea8
Dex Chen <dex.chen@commscope.com>
2025-06-13 21:51:30 +0800

    migrate to new providers config for avante.nvim

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -1,17 +1,24 @@
 return {
     {
         "yetone/avante.nvim",
+        enabled = true,
         event = "VeryLazy",
         version = false, -- Never set this value to "*"! Never!
         opts = {
             provider = "claude-haiku",
-            claude = {
-                api_key_name = "cmd:cat /home/dex/.anthropic-api-key",
-            },
-            --provider = "ollama",
-            ollama = {
-                model = "qwen2.5-coder:7b",
-                --model = "llama3",
+            providers = {
+                claude = {
+                    api_key_name = "cmd:cat /home/dex/.anthropic-api-key",
+                    model = "claude-3-5-haiku-20241022",
+                    extra_request_body = {
+                        max_tokens = 8192,
+                    },
+                },
+                --provider = "ollama",
+                ollama = {
+                    model = "qwen2.5-coder:7b",
+                    --model = "llama3",
+                },
             },
         },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
